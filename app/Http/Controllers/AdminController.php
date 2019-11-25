e490127a0f7210e24e35855c9f60b65fafa82e46
Ashiful Islam Prince <princeashifulislam@gmail.com>
2019-11-26 01:50:18 +0600

    first Commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin/admin_home');
+    }
+    public function show()
+    {
+        return view('admin/prince');
+    }
+}
